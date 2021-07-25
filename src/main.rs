23fcc28dab2f391dbd90f3b062f5c9272d343fea
LonnonjamesD <xxl3m0nk1ngxx@gmail.com>
2021-07-25 12:27:05 -0400

    Moved Code

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,80 @@
 pub mod colors;
 pub mod shapes;
 mod workings;
-mod renderer;
+mod window;
 
-use glium::{Display, glutin::{self, event_loop::EventLoop}, implement_vertex};
-use shapes::triangle::Triangle;
+use std::time;
 
-use crate::{renderer::Renderer, shapes::vector2::Vertex, workings::on_frame::on_frame};
+use colors::rgba::RGBA;
+use glium::{Surface, glutin::{self, event_loop::EventLoop}, implement_vertex};
+
+use crate::{shapes::{triangle::Triangle, vector2::Vertex}, window::Window};
+
+#[allow(dead_code)]
+impl Window {
+    fn on_load(self) {
+        
+    }
+    fn on_frame(mut self, event_loop: EventLoop<()>) {
+        let program = glium::Program::from_source(
+            &self.display, 
+            self.vertex_shader_src, 
+            self.fragment_shader_src, 
+            None)
+        .unwrap();
+        let bg_color = RGBA::new(241.0, 156.0, 187.0, 255.0).normalize();
+    
+        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    
+        
+
+        event_loop.run( move |event, _, control_flow| {
+            self.renderer.clear_dynamic();
+            let triangle = Triangle::new(
+                vec![-10,-10], 
+                vec![0,10], 
+                vec![10,-10]
+            ).to_vertices();
+            self.renderer.add_to_dynamic(triangle);
+            let vertex_buffer = glium::VertexBuffer::new(&self.display, &self.renderer.combine()).unwrap();
+    
+            let next_frame_time = time::Instant::now() +
+                time::Duration::from_nanos(16_666_667);
+            *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+    
+            match event {
+                glutin::event::Event::WindowEvent { event, .. } => match event {
+                    glutin::event::WindowEvent::CloseRequested => {
+                        *control_flow = glutin::event_loop::ControlFlow::Exit;
+                        return;
+                    },
+                    _ => return,
+                },
+                glutin::event::Event::NewEvents(cause) => match cause {
+                    glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                    glutin::event::StartCause::Init => (),
+                    _ => return,
+                },
+                _ => return,
+            }
+    
+            let mut target = self.display.draw();
+    
+            target = bg_color.set_bg_color(target);
+            target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,&Default::default()).unwrap();
+            target.finish().unwrap();
+        });
+    }
+}
 
 fn main() {
-    implement_vertex!(Vertex, position);
-    let mut renderer = Renderer::new();
-
-    let triangle = Triangle::new(
-        vec![-10,-10], 
-        vec![0,10], 
-        vec![10,-10]
-    ).to_vertices();
-    renderer.add_to_constant(triangle);
-    let shape = renderer.combine();
-
-    let (event_loop, display) = on_load();
-
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new()
+        .with_resizable(false)
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(1024.0, 768.0))
+        .with_title("Hello");
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
     let vertex_shader_src = r#"
         #version 140
@@ -42,15 +95,12 @@ fn main() {
         }
     "#;
 
-    on_frame(event_loop, display, vertex_shader_src, fragment_shader_src, shape);
+    implement_vertex!(Vertex, position);
+
+    let window = Window::new(
+        display,
+        vertex_shader_src,
+        fragment_shader_src
+    );
+    window.on_frame(event_loop);
 }
-
-fn on_load() -> (EventLoop<()>, Display) {
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-
-    return (event_loop, display)
-}
-
